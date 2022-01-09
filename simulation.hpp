6cf676f93f4d1d07db5139ad36bd87671f77ea4c
Giuseppe Bertolini <giuseppe.bertolini@studio.unibo.it>
2022-01-09 01:21:34 +0100

    implemening simulation.hpp

--- simulation.hpp
+++ simulation.hpp
@@ -1,121 +1,50 @@
-#ifndef SIMULAZIONE_HPP
-#define SIMULAZIONE_HPP
+#ifndef SIMULAZIONE_GRAPHYCH_HPP
+#define SIMULAZIONE_GRAPHYCH_HPP
 
 #include <vector>
 #include <iostream>
-//using s_, i_ r_ to represent 
-//susceptibles, infected and removed people
 
-enum class people {s_, i_, r_};
+
+enum class people{s_, i_, r_};
+
+//all the methods of the class world and all the free function declared
+//in this file are defined in simulation.cpp file
+//look in simulation.cpp file also for the description of
+//each method and free class funcionality
 
 class world 
 {
+    private:
     using grid = std::vector<people>;
     int m_side;
     grid m_grid;
 
-    //constructor
     public:
-    world(int n) 
-    : m_side(n), m_grid(m_side * m_side, people::s_)
-    {
-    }
+    //constructor
+    world(int n);
 
-    int side() const
-    {
-        return m_side;
-    }
+    //method size
+    int side() const;
 
-    std::vector<people> c_t()
-    {
-        return m_grid;
-    }
-    //commenta la conversione di indici
-    people const& person(int r, int c) const 
-    {
-        auto const i = (r + m_side) % m_side;
-        auto const j = (c + m_side) % m_side;
-        if (i < 0 || i > m_side || j < 0 || j > m_side) {
-            throw std::runtime_error{"unacceptable coordinates"};
-        }
-        auto const index = i * m_side + j;
-        if (index < 0 || index > static_cast<int>(m_grid.size())) {
-            throw std::runtime_error{"the conversion to monodimensional index went wrong "};
-        }
-        return m_grid[index];
-    }
+    //method people
+    people const& person(int r, int c) const;
 
-    people& person(int r, int c) 
-    {
-        auto const i = (r + m_side) % m_side;
-        auto const j = (c + m_side) % m_side;
-        if (i < 0 || i > m_side || j < 0 || j > m_side) {
-            throw std::runtime_error{"unacceptable coordinates"};
-        }
-        auto const index = i * m_side + j;
-        if (index < 0 || index > static_cast<int>(m_grid.size())) {
-            throw std::runtime_error{"the conversion to monodimensional index went wrong "};
-        }
-        return m_grid[index];
-    }
+    //overloading of method people
+    people& person(int r, int c);
+    
+    //definition of operator ==
+    //defines the operation of == between tho object
+    //of world type
     friend bool operator ==(world const& l, world const& r)
     {
         return l.m_grid == r.m_grid;
     }
 };
 
-//counter
-inline int neighbours_infected(world const& population_, int r, int c)
-{
-    int n_i = -static_cast<int>(population_.person(r, c));
-    if (population_.person(r, c) == people::r_) {n_i += 2;}
-    for (int i : {-1, 0, 1}) {
-        for (int j : {-1, 0, 1}) {
-            if (population_.person(r + i, c + j) == people::i_) {
-                n_i += 1;
-            }
-        }
-    }
-    return n_i;
-}
-
-inline world day_after(world const& present, double beta_, double gamma_)
-{   
-    if (beta_ <= 0 || beta_ >= 1) {
-        throw std::runtime_error{"beta's value is not acceptable"};
-    }
-    if (gamma_ <= 0 || gamma_ >= 1) {
-        throw std::runtime_error{"gamma's value is not acceptable"};
-    }
-    if (beta_ / gamma_ <= 1) {
-            throw std::runtime_error{"beta and gamma ratio is unacceptable"};
-        }
-    int const n = present.side();
-    world evolved(n);
-    for (int i = 0; i != n; ++i) { 
-        for (int j = 0; j != n; ++j) { 
-            int a = neighbours_infected(present, i, j);
-         if (beta_ >= 0.5 && gamma_ <= 0.5) {
-            a += 1;
-        }
-        int const& inf_ = a;
-        if (inf_ >= 2) {
-            if (present.person(i, j) == people::s_) {
-                evolved.person(i, j) = people::i_;
-            } else {
-                evolved.person(i, j) = people::r_;
-            }
-        } else {
-            if (present.person(i, j) == people::s_) {
-                evolved.person(i, j) = present.person(i, j);
-            } else {
-                evolved.person(i, j) = people::r_;
-            }
-        }
-        }       
-    }
-    return evolved;
-}
 
 
-#endif
+//definition of free function neighbours_infected
+int neighbours_infected(world const& population_, int r, int c);
+
+//definition of free function day_after
+world day_after(world const& present, double beta_, double gamma_);
